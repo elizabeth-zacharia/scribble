5eddeae366fe595466e44a32fd60fa78766b4adf
Elizabeth <elizabeth.zacharia.k@gmail.com>
2024-06-26 17:43:27 +0530

    addnot component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import './App.css';
 import Navbar from './components/navbar';
-import AddNote from './components/addNote';
 function App() {
   return <Navbar />;
-  return <AddNote />;
 }
-
 export default App;
